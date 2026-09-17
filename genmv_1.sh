b48a134bafcbd334f8f26fedde689e49c31c58a9
matteokoles <matteokole@gmail.com>
2026-09-17 13:26:21 +0200

    Fix: utilisation du chemin complet VBoxManage.exe (les alias ne fonctionnent pas dans un script non-interactif)

--- genmv_1.sh
+++ genmv_1.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-
 NOM="Debian1"
+VBOXMANAGE="/mnt/c/Program Files/Oracle/VirtualBox/VBoxManage.exe"
 
-VBoxManage createvm --name "$NOM" --ostype "Debian_64" --register
-
-VBoxManage modifyvm "$NOM" --memory 4096 --nic1 nat
-
-VBoxManage createmedium disk --filename "$NOM.vdi" --size 65536
-VBoxManage storagectl "$NOM" --name "SATA" --add sata --controller IntelAhci
-VBoxManage storageattach "$NOM" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "$NOM.vdi"
+"$VBOXMANAGE" createvm --name "$NOM" --ostype "Debian_64" --register
+"$VBOXMANAGE" modifyvm "$NOM" --memory 4096 --nic1 nat
+"$VBOXMANAGE" createmedium disk --filename "$NOM.vdi" --size 65536
+"$VBOXMANAGE" storagectl "$NOM" --name "SATA" --add sata --controller IntelAhci
+"$VBOXMANAGE" storageattach "$NOM" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "$NOM.vdi"
 
 echo "VM $NOM créée. Vérifiez dans l'interface VirtualBox."
 read -p "Appuyez sur Entrée pour détruire la VM..."
 
-# Destruction de la VM
-VBoxManage unregistervm "$NOM" --delete
+"$VBOXMANAGE" unregistervm "$NOM" --delete
 echo "VM $NOM détruite."
